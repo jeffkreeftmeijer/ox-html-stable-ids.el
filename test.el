4d2dc796cf15aeb8724b33b25709a17d1a8d9a9e
Jeff Kreeftmeijer <jeff@kreeft.me>
2023-05-03 00:50:22 +0200

    Add failing test for disabling

--- test.el
+++ test.el
@@ -4,16 +4,28 @@
 (ert-deftest hello-world-test ()
   (org-html-stable-ids-add)
   (find-file "test/fixtures/hello-world.org")
-  (org-html-export-as-html)
+  (let ((org-html-stable-ids t))
+    (org-html-export-as-html))
   (should (string-match-p
-           "<h2 id=\"hello-world"
-           (with-current-buffer "*Org HTML Export*" (buffer-string))))
+	   "<h2 id=\"hello-world"
+	   (with-current-buffer "*Org HTML Export*" (buffer-string))))
+  (org-html-stable-ids-remove))
+
+(ert-deftest disabled-test ()
+  (org-html-stable-ids-add)
+  (find-file "test/fixtures/hello-world.org")
+  (let ((org-html-stable-ids nil))
+    (org-html-export-as-html))
+  (should-not (string-match-p
+	       "<h2 id=\"hello-world"
+	       (with-current-buffer "*Org HTML Export*" (buffer-string))))
   (org-html-stable-ids-remove))
 
 (ert-deftest multiple-headlines-test ()
   (org-html-stable-ids-add)
   (find-file "test/fixtures/multiple-headlines.org")
-  (org-html-export-as-html)
+  (let ((org-html-stable-ids t))
+    (org-html-export-as-html))
   (let ((buffer (with-current-buffer "*Org HTML Export*" (buffer-string))))
     (should (string-match-p "<h2 id=\"hello-world" buffer))
     (should (string-match-p "<h2 id=\"another-headline" buffer)))
@@ -22,7 +34,8 @@
 (ert-deftest src-block-test ()
   (org-html-stable-ids-add)
   (find-file "test/fixtures/src-block.org")
-  (org-html-export-as-html)
+  (let ((org-html-stable-ids t))
+    (org-html-export-as-html))
   (let ((buffer (with-current-buffer "*Org HTML Export*" (buffer-string))))
     (should (string-match-p "<pre class=\"src src-shell\">" buffer)))
   (org-html-stable-ids-remove))
@@ -30,7 +43,8 @@
 (ert-deftest example-block-test ()
   (org-html-stable-ids-add)
   (find-file "test/fixtures/example-block.org")
-  (org-html-export-as-html)
+  (let ((org-html-stable-ids t))
+    (org-html-export-as-html))
   (let ((buffer (with-current-buffer "*Org HTML Export*" (buffer-string))))
     (should (string-match-p "<pre class=\"example\">" buffer)))
   (org-html-stable-ids-remove))
@@ -38,11 +52,15 @@
 (ert-deftest duplicate-headlines-test ()
   (org-html-stable-ids-add)
   (find-file "test/fixtures/duplicate-headlines.org")
-  (should-error (org-html-export-as-html))
+  (should-error
+   (let ((org-html-stable-ids t))
+     (org-html-export-as-html)))
   (org-html-stable-ids-remove))
 
 (ert-deftest duplicate-headlines-with-custom-id-test ()
   (org-html-stable-ids-add)
   (find-file "test/fixtures/duplicate-headlines-with-custom-id.org")
-  (should-error (org-html-export-as-html))
+  (should-error
+   (let ((org-html-stable-ids t))
+     (org-html-export-as-html)))
   (org-html-stable-ids-remove))
